ed94d8101825ad26a1c7c7003c73aed2716890d7
Tero Parviainen <tero.parviainen@eficode.fi>
2014-01-06 12:08:55 +0200

    Enable Unit Testing With Jasmine

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,18 +2,34 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     jshint: {
-      all: ['src/**/*.js'],
+      all: ['src/**/*.js', 'test/**/*.js'],
       options: {
         globals: {
           _: false,
-          $: false
+          $: false,
+          jasmine: false,
+          describe: false,
+          it: false,
+          expect: false,
+          beforeEach: false,
+          waits: false,
+          runs: false
         },
         browser: true,
         devel: true
+      }
+    },
+    jasmine: {
+      unit: {
+        src: 'src/**/*.js',
+        options: {
+          specs: ['test/**/*.js']
+        }
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
 
 };
