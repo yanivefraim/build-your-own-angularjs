b4770fa94fb530f85fd7326b152e70878db25ad5
Tero Parviainen <tero.parviainen@eficode.fi>
2014-01-06 12:24:41 +0200

    Watching Object Properties: $watch And $digest

--- src/scope.js
+++ src/scope.js
@@ -2,5 +2,19 @@
 'use strict';
 
 function Scope() {
-
+	this.$$watchers = [];
 }
+
+Scope.prototype.$watch = function(watchFn, listenerFn) {
+  var watcher = {
+    watchFn: watchFn,
+    listenerFn: listenerFn
+  };
+  this.$$watchers.push(watcher);
+};
+
+Scope.prototype.$digest = function() {
+  this.$$watchers.forEach(function(watcher) {
+    watcher.listenerFn();
+  });
+};
