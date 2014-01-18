cbb3c58f22d2dad986ab73480919c09866271f22
Tero Parviainen <tero.parviainen@eficode.fi>
2014-01-18 21:36:59 +0200

    Setting Up The Infrastructure for Watching Collections

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -760,4 +760,14 @@ describe("Scope", function() {
 
   });
 
+  describe("$watchCollection", function() {
+    
+    var scope;
+
+    beforeEach(function() {
+      scope = new Scope();
+    });
+  
+  });
+
 });
